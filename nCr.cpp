a2068ba6b9ab863ed69c7baff53ade407279bfdd
Unnati Sadh <82529288+Unnati-Sadh@users.noreply.github.com>
2023-01-12 15:53:33 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int factorial(int num){
   int fact = 1;
-  for(int i=0; i<=num; i++){
-  
+  for(int i=1; i<=num; i++){
+    fact = fact*i;
   }
+  return fact;
+}
+int nCr(int n, int r){
+  int ans = factorial(n)/(factorial(r)*(factorial(n-r)));
+  return ans;
+}
+int main() {
+  int n,r;
+  cin>> n >> r;
+  cout << "Answer is: " << nCr(n,r);
+  return 0;
 }
