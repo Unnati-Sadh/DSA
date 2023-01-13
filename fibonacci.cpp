8f39f08aaaadf6b9d352809c7b11ff2db92e350c
Unnati Sadh <82529288+Unnati-Sadh@users.noreply.github.com>
2023-01-13 13:23:37 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,1 +1,22 @@
+//nth term of fibonacci series 
 
+
+#include<iostream>
+using namespace std;
+
+int nthTerm(int n){
+    int a = 0;
+    int b = 1;
+  for(int i = 2; i<n; i++){
+      int next = a + b;
+      a = b;
+      b = next;
+  }
+  return b;
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << "The " << n << "th term is " << nthTerm(n);
+  return 0;
+}
