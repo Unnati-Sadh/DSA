e1ef8f3e9186c629b8ef7e7957c2520cb83f048a
Unnati Sadh <82529288+Unnati-Sadh@users.noreply.github.com>
2023-01-17 20:39:07 +0530

    Update arrElementsSum.cpp

--- arrElementsSum.cpp
+++ arrElementsSum.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+
+int arrSum(int arr[],int n){
+    int sum = 0;
+  for(int i = 0; i<=n; i++){
+      sum = sum + i;
+  }
+  return sum;
+}
+
+int main(){
+    int size, arr1[100];
+    cin >> size;
+  for(int i = 1; i<size; i++){
+    cin >> arr1[i];
+  }
+  cout << "Sum of all digits: " << arrSum(arr1, size);
+  
+  return 0;
+}
