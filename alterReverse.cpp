87f5fa709f370974b072f08ba7573daf1eb0cf21
Unnati Sadh <82529288+Unnati-Sadh@users.noreply.github.com>
2023-01-23 21:56:57 +0530

    Create alterReverse.cpp

---
+++ alterReverse.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void alterReverse(int arr[], int size){
+    for(int i = 0; i<size; i+=2){
+        if(i+1<size){
+          swap(arr[i],arr[i+1]);
+        }
+    }
+}
+void printArray(int arr[], int size){
+    for(int i = 0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    
+  int even[6]={1,2,3,4,5,6};
+  int odd[5]={1,2,3,4,5};
+  
+  alterReverse(even, 6);
+  alterReverse(odd, 5);
+  
+  printArray(even, 6);
+  cout << endl;
+  printArray(odd, 5);
+  
+  return 0;
+  
+}
